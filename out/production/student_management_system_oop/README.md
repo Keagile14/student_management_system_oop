13e2a2b02b76435c944d5f056673038438831419
Keagile14 <kendulijhb024@student.wethinkcode.co.za>
2025-06-05 11:05:32 +0200

    updated StudentManager class

---
+++ out/production/student_management_system_oop/README.md
@@ -0,0 +1,9 @@
+Student Management System(Practice OOP)
+
+Concepts: Classes, objects, constructors, lists, basic methods.
+
+    Create a Student class with name, studentId, grade.
+
+    Create a StudentManager class to add, find, and list students.
+
+    Practice using ArrayList<Student>.
--- system/main.java
+++ system/main.java
@@ -1,4 +1,16 @@
 package system;
 
 public class main {
+
+    public static void main(String[] args){
+        StudentManager manager = new StudentManager();
+
+        manager.addStudent("Keagile",405, 15);
+        manager.addStudent("Sean",103,11);
+        manager.addStudent("Mandisi",406,11);
+
+
+        System.out.println("\n All students:");
+        manager.listStudent();
+    }
 }
